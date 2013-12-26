f6a359dfe94df4b8dc4c279b3a8ced34f151ce04
Mateusz Juściński <m.juscinski@gmail.com>
2013-12-26 13:17:40 +0100

    + config file

---
+++ js/app/config.js
@@ -0,0 +1,15 @@
+(function(oDeezerpp)
+{
+	// application config
+	oDeezerpp.constant(
+		'AppSettings',
+		{
+			dz: {
+				appid:		123285,
+				channel:	'http://luinnar.github.io/DPlusPlus/channel.html'
+			},
+			debug: false
+		}
+	);
+	
+})(oDeezerpp);
