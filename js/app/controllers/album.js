7708264e2da13752b426e102ff9af1538ce9d65c
Mateusz Juściński <m.juscinski@gmail.com>
2013-12-29 04:00:03 +0100

    ~ new feature: add single track to playlist ~ small view fixes
    music: Best of Gotan Project by Gotan Project

--- js/app/controllers/album.js
+++ js/app/controllers/album.js
@@ -2,6 +2,8 @@
 {
 	function AlbumController($scope, $routeParams, oDeezerApi, oPlaylist, AlbumFilter)
 	{
+		var _oFiltered = null;
+		
 		/**
 		 * Add album at end of a playlist
 		 */
@@ -9,7 +11,7 @@
 		{
 			$event.preventDefault();
 			
-			var oTmp = AlbumFilter($scope.album);
+			var oTmp = _getFiltered();
 			oPlaylist.add(oTmp.tracks, oTmp.albums, oTmp.artists);
 		};
 		
@@ -20,8 +22,42 @@
 		{
 			$event.preventDefault();
 			
-			var oTmp = AlbumFilter($scope.album);
+			var oTmp = _getFiltered();
 			oPlaylist.play(oTmp.tracks, oTmp.albums, oTmp.artists);
+		};
+		
+		/**
+		 * Add song at end of a playlist
+		 */
+		$scope.playSingleLast = function(iPos, $event)
+		{
+			$event.preventDefault();
+			
+			var oTmp = _getFiltered();
+			
+			if(!oTmp.tracks[iPos])
+			{
+				return;
+			}
+
+			oPlaylist.add([oTmp.tracks[iPos]], oTmp.albums, oTmp.artists);
+		};
+		
+		/**
+		 * Plays track instantly
+		 */
+		$scope.playSingleNow = function(iPos, $event)
+		{
+			$event.preventDefault();
+
+			var oTmp = _getFiltered();
+
+			if(!oTmp.tracks[iPos])
+			{
+				return;
+			}
+
+			oPlaylist.play([oTmp.tracks[iPos]], oTmp.albums, oTmp.artists);
 		};
 		
 		/**
@@ -35,6 +71,21 @@
 				{
 					$scope.album = oData;
 				});
+		}
+		
+		/**
+		 * Returns data prepared for playlist service
+		 * 
+		 * @returns	{Object}
+		 */
+		function _getFiltered()
+		{
+			if(_oFiltered === null)
+			{
+				_oFiltered = AlbumFilter($scope.album);
+			}
+			
+			return _oFiltered;
 		}
 		
 		_init();
